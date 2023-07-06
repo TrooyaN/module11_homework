810105c931fe61ff32c12967eff4b3626763639c
TrooyaN <133668413+TrooyaN@users.noreply.github.com>
2023-07-06 15:06:34 +0500

    Delete JS модуль 11 directory

--- JS модуль 11/Задание 3.js
+++
@@ -1,8 +0,0 @@
-function func1(num) {
-    function func2(num2) {
-        return num + num2;
-    }
-    return func2(2);
-}
-
-console.log(func1(1))
--- JS модуль 11/Задание 5.js
+++
@@ -1,6 +0,0 @@
-const func = (first, second) => {
-    let result = Math.pow(first, second);
-    console.log(result);
-  }
-  
-  func(2, 5);
